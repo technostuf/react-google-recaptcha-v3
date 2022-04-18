d41e5b8feb4d8f2cc2d4d619063c65660ecd87e8
Amit Patel <89689746+technostuf@users.noreply.github.com>
2022-04-18 14:54:43 +0530

    google recaptcha v3 example

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SpinnerComponent from './SpinnerComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <SpinnerComponent />
+    </div>
+  );
+}
+export default App;
